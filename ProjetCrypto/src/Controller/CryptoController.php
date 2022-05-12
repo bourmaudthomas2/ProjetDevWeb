ab1f65f42930a8ae7eae5a4aa1308a717bd35b06
bourmaudthomas2 <bourmaud.thomas2@gmail.com>
2022-05-12 15:12:33 +0200

    Commit

--- ProjetCrypto/src/Controller/CryptoController.php
+++ ProjetCrypto/src/Controller/CryptoController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CryptoController extends AbstractController
 {
     /**
-     * @Route("/crypto", name="app_crypto")
+     * @Route("/crypto", name="crypto.index")
      */
     public function index(): Response
     {
@@ -23,15 +23,37 @@ class CryptoController extends AbstractController
     }
 
     /**
-     * @Route("/crypto/load", name="app_crypto_load")
+     * @Route("/crypto/load", name="crypto.load")
      */
     public function cryptoLoad(): Response
     {
-
-
         return $this->render('crypto/index.html.twig', [
             'controller_name' => 'CryptoController',
         ]);
+    }
 
+    /**
+     * @Route("/crypto/detail/{id}", name="crypto.detail")
+     * @return Response
+     */
+    public function cryptoOneById($id): Response
+    {
+
+        $res = $this->getDoctrine()->getRepository(Crypto::class)->find($id);
+        $crypto['id']=$res->getId();
+        $crypto['nom']=$res->getNom();
+        $crypto['symbole']=$res->getSymbole();
+        $crypto['description']=$res->getDescription();
+        $crypto['prix']=$res->getPrix();
+        $crypto['marketcap']=$res->getMarketcap();
+        $crypto['categorie']=$res->getCategorie();
+        $crypto['followers']=$res->getFollowers();
+        $crypto['vote_up']=$res->getVoteUp();
+        $crypto['date_creation']=$res->getDateCreation();
+
+
+        return $this->render('crypto/detail.html.twig', [
+            'crypto' => $crypto,
+        ]);
     }
 }
