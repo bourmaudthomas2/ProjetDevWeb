ce1048132188df4a2f63f6089c9607a7421324e0
Malsoine <tom.barillet@hotmail.fr>
2022-05-14 16:15:50 +0200

    Ajout de la création des commentaires

--- ProjetCrypto/src/Controller/CommentaireController.php
+++ ProjetCrypto/src/Controller/CommentaireController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Commentaire;
+use App\Entity\User;
+use App\Entity\Crypto;
+use App\Form\CommentaireType;
 
 class CommentaireController extends AbstractController
 {
@@ -17,4 +23,35 @@ class CommentaireController extends AbstractController
             'controller_name' => 'CommentaireController',
         ]);
     }
+    /**
+     *
+     * @Route("/add/comm/{id}", name="ajoutComm")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return RedirectResponse|Response
+     */
+    public function ajoutComm(Request $request, EntityManagerInterface $em, $id):Response
+    {
+        $commentaire = new Commentaire();
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(array('username' => $this->getUser()->getUsername()));
+        $crypto = $this->getDoctrine()->getRepository(Crypto::class)->findOneBy(array('id'=>$id));
+        $commentaire->setUser($user);
+        $commentaire->setCrypto($crypto);
+
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($commentaire);
+            $user->addCommentaire($commentaire);
+            $crypto->addCommentaire($commentaire);
+
+            $em->flush();
+            return $this->redirectToRoute('crypto.index');
+
+        }
+        return $this->render('commentaire/index.html.twig', [
+            'form' => $form->createView(), ]);
+
+    }
+
 }
