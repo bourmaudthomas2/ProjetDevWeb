18052e04873b234a5486cf98a931b2a2d6007896
Malsoine <tom.barillet@hotmail.fr>
2022-05-18 09:52:23 +0200

    trad

---
+++ ProjetCrypto/src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/change-locale/{locale}", name="change_locale")
+     */
+    public function changeLocale($locale, Request $request)
+    {
+        $request->getSession()->set('_locale',$locale);
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
